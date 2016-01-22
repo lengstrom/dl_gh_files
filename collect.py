4e90bb08f8d7d2aa686d622b99d52f7a38723d13
lengstrom <lengstrom@users.noreply.github.com>
2016-01-22 18:04:01 -0500

    added download_range

--- collect.py
+++ collect.py
@@ -5,10 +5,20 @@ s3 = boto3.resource('s3')
 def get_buckets():
     return filter(lambda x :'authors' in x.name, list(s3.buckets.all()))
 
+def get_df(path):
+    store = pd.HDFStore(path, 'r')
+    df = store['df']
+    store.close()
+    return df
+
 if __name__ == "__main__":
     buckets = get_buckets()
     archive_dir = './archive'
-    if False:
+    _, tokens_path, num_instances = sys.argv
+    #tokens = get_df(tokens_path)
+    
+    out_fp = os.path.join("all_files", 'all_files.h5')
+    if not os.path.exists(out_fp):
         if not os.path.exists(archive_dir):
             os.mkdir(archive_dir)
         for bucket in buckets:
@@ -20,14 +30,44 @@ if __name__ == "__main__":
                 if not os.path.exists(obj_file):
                     bucket.download_file(obj.key, obj_file)
 
-    dfs = []
-    for i in os.listdir(archive_dir):
-        cat = os.path.join(archive_dir, i)
-        if os.path.isdir(cat):
-            for csv in os.listdir(cat):
-                dfs.append(pd.read_csv(os.path.join(cat, csv)))
+        dfs = []
+        for i in os.listdir(archive_dir):
+            cat = os.path.join(archive_dir, i)
+            if os.path.isdir(cat):
+                for csv in os.listdir(cat):
+                    dfs.append(pd.read_csv(os.path.join(cat, csv)))
 
-    end = pd.concat(dfs)
-    store = pd.HDFStore(os.path.join(archive_dir, 'all_files.h5'))
-    store.put('df', end, format='table')
-    store.close()
+        end = pd.concat(dfs)
+        store = pd.HDFStore(out_fp)
+        store.put('df', end, format='table')
+        store.close()
+    store = pd.HDFStore(out_fp)
+    file_list = store['df']['html']
+    
+    num_instances = int(num_instances)
+    num_files = file_list.shape[0]
+    num_per_subgroup = int(math.ceil(float(num_files)/num_instances))
+
+    subgroups = [file_list.iloc[i * num_per_subgroup:(i+1) * num_per_subgroup] for i in range(num_instances)]
+
+    out_dir = "./out"
+    files_out = []
+    bucket = s3.Bucket('ghallfiles')
+    for n, v in enumerate(subgroups):
+        try:
+            key = "files_%s" % n
+            file_to_make = os.path.join(out_dir, key)
+            if os.path.exists(file_to_make):
+                os.remove(file_to_make)
+            store = pd.HDFStore(file_to_make)
+            files_out.append(file_to_make)
+            store['df'] = v
+            store.close()
+
+            with open(file_to_make, 'rb') as f:
+                bucket.put_object(Key=key, Body=f)
+
+        except:
+            pdb.set_trace()
+
+    print files_out
---
+++ download_range.py
@@ -0,0 +1,66 @@
+import pandas as pd, math, boto3, sys, os, subprocess, pdb, time, requests, zipfile, codecs, urllib, unicodedata
+from subprocess import Popen
+s3 = boto3.resource('s3')
+
+def archive_directory(path, Key, bucket):
+    zipf = zipfile.ZipFile('archive.zip', 'w')
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            f_path = os.path.join(root, file)
+            zipf.write(f_path)
+            os.remove(f_path)
+    zipf.close()
+    with open('archive.zip', 'rb') as f:
+        bucket.put_object(Key=Key, Body=f)
+    os.remove('archive.zip')
+
+def strip_utf8(string):
+    return unicodedata.normalize('NFKD', string).encode('ascii','ignore')
+    
+if __name__ == "__main__":
+    _, files_key = sys.argv
+
+    try:
+        s3.meta.client.head_bucket(Bucket=files_key)
+    except Exception as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+#        error_code = int(e.response['Error']['Code'])
+ #       if error_code == 404:
+        s3.create_bucket(Bucket=files_key)
+        #else:
+         #   raise Exception('Other error')
+
+    bucket = s3.Bucket(files_key)
+    downloaded_files = [int(i.key) for i in bucket.objects.page_size(1000000)]
+
+    if len(downloaded_files) == 0:
+        max_so_far = 0
+    else:
+        max_so_far = max(downloaded_files)
+
+    to_download_path = files_key + ".h5"
+    if not os.path.exists(to_download_path):
+        s3.Bucket('ghallfiles').download_file(files_key, to_download_path)
+
+    store = pd.HDFStore(to_download_path)
+    files = store['df']
+
+    downloaded_dir = 'downloaded'
+    for i in xrange(max_so_far, files.shape[0]):
+        if i % 100 == 0 and i != 0:
+            archive_directory(downloaded_dir, str(i), bucket)
+        print i
+        curr_url = files.iloc[i]
+        end_of_url = curr_url.replace('/blob/', '/')[18:].replace('?',"%3F").replace('#', '%23')
+        to_get_url = "https://raw.githubusercontent.com" + end_of_url
+        r = requests.get(to_get_url)
+        if r.status_code != 200:
+            print "Problem!"
+            print r.headers
+            pdb.set_trace()
+
+        file_name = strip_utf8(end_of_url).replace('/', '__')
+        with codecs.open(os.path.join(downloaded_dir, file_name), 'w', encoding='utf-8') as f:
+            f.write(r.text)
+    
