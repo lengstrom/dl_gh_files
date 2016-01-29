cf276bafb039afb465bbd119f79a0d4e388adb89
lengstrom <lengstrom@users.noreply.github.com>
2016-01-29 05:50:31 -0500

    addeed

--- analyze_dl_files.py
+++ analyze_dl_files.py
@@ -6,6 +6,8 @@ store = pd.HDFStore(fp)
 df = store['df'].drop([u'index', u'api', u'fork'],axis=1)
 store.close()
 
+filters = ['bower', 'node_modules', 'bench.sh', 'Pods', 'mongo', 'cordova']
+
 bash_histories = df[df['html'].str.lower().str.contains('bash_history')]
 zsh_histories = df[df['html'].str.lower().str.contains('zsh_history')]
 #histories = df[df['html'].str.lower().str.contains('_history')]
--- download_range.py
+++ download_range.py
@@ -3,16 +3,41 @@ from subprocess import Popen
 s3 = boto3.resource('s3')
 
 def archive_directory(path, Key, bucket):
-    zipf = zipfile.ZipFile('archive.zip', 'w')
+    archive_path = 'archive.zip'
+    zipf = zipfile.ZipFile(archive_path, 'w')
+    print "archiving %s to %s" % (Key, archive_path)
     for root, dirs, files in os.walk(path):
         for file in files:
             f_path = os.path.join(root, file)
             zipf.write(f_path)
             os.remove(f_path)
     zipf.close()
-    with open('archive.zip', 'rb') as f:
+    with open(archive_path, 'rb') as f:
         bucket.put_object(Key=Key, Body=f)
-    os.remove('archive.zip')
+    os.remove(archive_path)
+
+def complete_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        code = r.status_code
+        if code == 404:
+            return None
+
+        if code == 503:
+            print "503"
+            time.sleep(5)
+            return complete_url(url)
+
+        print "Problem!"
+        print r.headers
+        pdb.set_trace()
+    else:
+         return r   
+
+def trim_gh_file(path):
+    for i in range(4):
+        path = path[path.index('/')+1:]
+    return path
 
 def strip_utf8(string):
     return unicodedata.normalize('NFKD', string).encode('ascii','ignore')
@@ -47,19 +72,22 @@ if __name__ == "__main__":
     files = store['df']
 
     downloaded_dir = 'downloaded'
+    n = 0
+    [os.remove(os.path.join(downloaded_dir, i)) for i in os.listdir(downloaded_dir)]
     for i in xrange(max_so_far, files.shape[0]):
-        if i % 100 == 0 and i != 0:
-            archive_directory(downloaded_dir, str(i), bucket)
-        print i
+        if n % 100 == 0 and n != 0:
+            archive_directory(downloaded_dir, str(i-1), bucket)
         curr_url = files.iloc[i]
         end_of_url = curr_url.replace('/blob/', '/')[18:].replace('?',"%3F").replace('#', '%23')
         to_get_url = "https://raw.githubusercontent.com" + end_of_url
-        r = requests.get(to_get_url)
-        if r.status_code != 200:
-            print "Problem!"
-            print r.headers
-            pdb.set_trace()
-
-        file_name = strip_utf8(end_of_url).replace('/', '_||_')
-        with codecs.open(os.path.join(downloaded_dir, file_name), 'w', encoding='utf-8') as f:
-            f.write(r.text)
+        file_name = trim_gh_file(end_of_url).replace('/', '_||_')[-255:]
+        path_to_dl = os.path.join(downloaded_dir, file_name)
+        print i, file_name
+        if not os.path.exists(path_to_dl):
+            r = complete_url(to_get_url)
+            if r == None:
+                continue
+          #  file_name = strip_utf8(end_of_url).replace('/', '_||_')
+            with codecs.open(path_to_dl, 'w', encoding='utf-8') as f:
+                f.write(r.text)
+        n += 1
